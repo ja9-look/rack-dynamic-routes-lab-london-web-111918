64335aa52e514cddefdef339349cd803b03f9c9a
Janine Luk <janineluk95@gmail.com>
2018-12-10 15:29:39 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    
+    if req.path =="/items"
+      resp.write "#{items.price}"
+    elsif
+      resp.write "Item not found"
+      resp.status = 400
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    
+    resp.finish
+  end
+  
+end
