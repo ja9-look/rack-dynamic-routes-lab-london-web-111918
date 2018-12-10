2b0df4ef75d6bf61b8a185f9296bd2184d2f5cba
Janine Luk <janineluk95@gmail.com>
2018-12-10 15:00:11 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    
+    if req.path=="/items"
+      resp.write "#{item.price}"
+    else
+      resp.write "Item not found"
+      resp.status = 404
+    end
+    
+    resp.finish
+  end
+  
+end
